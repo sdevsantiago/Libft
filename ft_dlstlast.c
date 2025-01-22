0bcc57280d6b0e78d23cedad71947f86dc9ebcd8
Sergio <147100454+sdevsantiago@users.noreply.github.com>
2025-01-22 19:56:17 +0100

    🙃 fix: i'm stupid

--- ft_dlstlast.c
+++ ft_dlstlast.c
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/19 16:46:34 by sede-san          #+#    #+#             */
-/*   Updated: 2025/01/22 19:21:08 by sede-san         ###   ########.fr       */
+/*   Updated: 2025/01/22 19:53:11 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,7 @@ t_dlist	*ft_dlstlast(t_dlist *lst)
 {
 	if (!lst)
 		return (NULL);
-	return (lst->previous);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
 }
