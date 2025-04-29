c4e5959cd0c57ad7f1db4c0cc075ad1636b3cb70
sede-san <sede-san@student.42madrid.com>
2025-04-29 17:22:05 +0200

    fix: fixed errors.h inline function so that it always returns a string (may be an empty string if 0 is passed)

--- errors.h
+++ errors.h
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 19:22:28 by sede-san          #+#    #+#             */
-/*   Updated: 2025/04/24 20:19:57 by sede-san         ###   ########.fr       */
+/*   Updated: 2025/04/29 17:20:26 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 #ifndef ERRORS_H
 # define ERRORS_H
 
+# define NO_ERRORS_MESSAGE ""
 # define ERROR_ARGS_MESSAGE "ERROR: Not enough arguments"
 
 typedef enum e_errors
 {
+	NO_ERROR = 0,
 	ERROR_ARGS = 1
 }			t_errors;
 
@@ -28,6 +30,8 @@ static inline char	*ft_geterror(t_errors error_code)
 
 	if (error_code == ERROR_ARGS)
 		error = ERROR_ARGS_MESSAGE;
+	else
+		error = NO_ERRORS_MESSAGE;
 	return (error);
 }
 
