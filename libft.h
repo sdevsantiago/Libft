bafb863eb84eb6f2e7b7d41926308643c0b1f528
Sergio <sergio.desantiago@hotmail.es>
2025-08-03 01:57:36 +0200

    update: linux/limits.h is included instead of limits.h if user is in Linux (necessary for minishell)

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 17:40:58 by sede-san          #+#    #+#             */
-/*   Updated: 2025/08/01 19:47:53 by sede-san         ###   ########.fr       */
+/*   Updated: 2025/08/03 01:56:42 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,11 @@
 # include <stddef.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <limits.h>
+# ifdef __unix__
+#  include <linux/limits.h>
+# else
+#  include <limits.h>
+# endif
 # include <unistd.h>
 # include "errors.h"
 # include "colors.h"
