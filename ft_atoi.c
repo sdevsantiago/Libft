99415df1f0a28128fa505ff5ed42b588e7b9eae0
Sergio <sergio.desantiago@hotmail.es>
2025-08-24 02:34:16 +0200

    update: ft_atoi now calls ft_atoi_base

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 17:37:46 by sede-san          #+#    #+#             */
-/*   Updated: 2024/10/08 14:30:20 by sede-san         ###   ########.fr       */
+/*   Updated: 2025/08/24 19:04:21 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,5 @@
 /* Converts a string into an integer.  */
 int	ft_atoi(const char *str)
 {
-	int	minus;
-	int	num;
-
-	minus = 1;
-	num = 0;
-	while (ft_isspace(*str))
-		str++;
-	if (*str == '-' || *str == '+')
-	{
-		if (*str == '-')
-			minus = -minus;
-		str++;
-	}
-	while (*str >= '0' && *str <= '9')
-	{
-		num = num * 10 + (*str - '0');
-		str++;
-	}
-	return (num * minus);
+	return (ft_atoi_base(str, DEC_BASE));
 }
